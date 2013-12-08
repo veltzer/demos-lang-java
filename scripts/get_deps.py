ca582599bd00685a18c60c35afd8a76629b62408
Mark Veltzer <mark.veltzer@gmail.com>
2013-12-08 15:35:56 +0200

    small fix to the get_deps.py script

--- scripts/get_deps.py
+++ scripts/get_deps.py
@@ -4,46 +4,53 @@ import subprocess
 import shutil # for rmtree, copyfile
 import os.path # for join, split
 import os # for mkdir
+import glob # for glob
 
 shutil.rmtree('static', True)
 os.mkdir('static')
 
+version='3.6.1.v20120521-2329'
+
 subprocess.check_call(['wget','http://maven.repository.paxle.net/org/jdesktop/jdic/jdic/0.9.5/jdic-0.9.5.jar','-O','static/jdic.jar'])
 
 l=[
-	'org.eclipse.core.commands_3.6.2.v20130123-162658.jar',
-	'org.eclipse.core.contenttype_3.4.200.v20120523-2004.jar',
-	'org.eclipse.core.filebuffers_3.5.200.v20120523-1310.jar',
-	'org.eclipse.core.jobs_3.5.300.v20120912-155018.jar',
-	'org.eclipse.core.resources_3.8.1.v20121114-124432.jar',
-	'org.eclipse.core.runtime_3.8.0.v20120912-155025.jar',
-	'org.eclipse.debug.core_3.7.100.v20120521-2012.jar',
-	'org.eclipse.e4.ui.workbench3_0.12.0.v20120521-2329.jar',
-	'org.eclipse.equinox.app_1.3.100.v20120522-1841.jar',
-	'org.eclipse.equinox.common_3.6.100.v20120522-1841.jar',
-	'org.eclipse.equinox.preferences_3.5.1.v20121031-182809.jar',
-	'org.eclipse.equinox.registry_3.5.200.v20120522-1841.jar',
-	'org.eclipse.jdt.launching_3.6.101.v20130111-183046.jar',
-	'org.eclipse.jface_3.8.102.v20130123-162658.jar',
-	'org.eclipse.jface.text_3.8.2.v20121126-164145.jar',
-	'org.eclipse.osgi_3.8.2.v20130124-134944.jar',
-	'org.eclipse.persistence.jpa.equinox.weaving_2.4.1.v20121003-ad44345.jar',
-	'org.eclipse.swt_3.100.1.v4236b.jar',
-	'org.eclipse.swt.gtk.linux.x86_3.100.1.v4236b.jar',
-	'org.eclipse.text_3.5.200.v20120523-1310.jar',
-	'org.eclipse.ui_3.104.0.v20121024-145224.jar',
-	'org.eclipse.ui.editors_3.8.0.v20120523-1540.jar',
-	'org.eclipse.ui.forms_3.5.200.v20120705-114351.jar',
-	'org.eclipse.ui.ide_3.8.2.v20121106-165923.jar',
-	'org.eclipse.ui.views_3.6.100.v20120705-114010.jar',
-	'org.eclipse.ui.workbench_3.104.0.v20130204-164612.jar',
-	'org.eclipse.ui.workbench.texteditor_3.8.0.v20120523-1310.jar',
-	'org.eclipse.wst.server.core_1.4.100.v20130117_1140.jar',
+	'org.eclipse.core.commands',
+	'org.eclipse.core.contenttype',
+	'org.eclipse.core.filebuffers',
+	'org.eclipse.core.jobs',
+	'org.eclipse.core.resources',
+	'org.eclipse.core.runtime',
+	'org.eclipse.debug.core',
+	'org.eclipse.e4.ui.workbench3',
+	'org.eclipse.equinox.app',
+	'org.eclipse.equinox.common',
+	'org.eclipse.equinox.preferences',
+	'org.eclipse.equinox.registry',
+	'org.eclipse.jdt.launching',
+	'org.eclipse.jface',
+	'org.eclipse.jface.text',
+	'org.eclipse.osgi',
+	#'org.eclipse.persistence.jpa.equinox.weaving',
+	'org.eclipse.swt',
+	'org.eclipse.swt.gtk.linux.x86',
+	'org.eclipse.text',
+	'org.eclipse.ui',
+	'org.eclipse.ui.editors',
+	'org.eclipse.ui.forms',
+	'org.eclipse.ui.ide',
+	'org.eclipse.ui.views',
+	'org.eclipse.ui.workbench',
+	'org.eclipse.ui.workbench.texteditor',
+	'org.eclipse.wst.server.core',
 ]
 
 epath='/home/mark/install/eclipse-jee/plugins'
 for x in l:
-	shutil.copyfile(os.path.join(epath,x),os.path.join('static',x))
+	candidates=glob.glob(os.path.join(epath,x+'_*.jar'))
+	if len(candidates)!=1:
+		raise ValueError('too many candidates for '+x)
+	filename=candidates[0]
+	shutil.copyfile(filename,os.path.join('static',os.path.split(filename)[1]))
 
 files=[
 	'/usr/share/java/jna.jar',
