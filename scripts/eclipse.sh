527343380ba35b6f731e6cd7e9b2c65e56d4a0a6
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-03 19:50:52 +0300

    more changes

--- scripts/eclipse.sh
+++ scripts/eclipse.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# before running this script make sure that you have eclipse for java installed
+# before running this script make sure that you have eclipse installed
 # in ~/install/eclipse-XXX (may be soft link).
-~/install/eclipse-jee/eclipse -data ~/workspace-jee &
-#~/install/eclipse-java/eclipse -data ~/workspace-java &
-#~/install/eclipse-rcp/eclipse -data ~/workspace-java &
+VERSION=jee
+#VERSION=java
+#VERSION=rcp
+if test -d ~/workspace-$VERSION -a -f ~/install/eclipse-$VERSION/eclipse; then
+	~/install/eclipse-$VERSION/eclipse -data ~/workspace-$VERSION 2> /dev/null 1> /dev/null &
+else
+	echo "you have a problem with the version [$VERSION]..."
+fi
