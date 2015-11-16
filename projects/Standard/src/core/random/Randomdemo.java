2b0d31a70a40641e6ac43010b18e8550eff2b7b8
Mark Veltzer <mark.veltzer@gmail.com>
2015-11-16 15:26:36 +0200

    another demo

---
+++ projects/Standard/src/core/random/Randomdemo.java
@@ -0,0 +1,20 @@
+package core.random;
+
+import java.util.Random;
+
+/*
+ * This example shows the default java.util.Random class
+ * and it's workings.
+ * It is based on a 48 bit internals state as per Donald Knuth's TAOCP.
+ */
+public class Randomdemo {
+
+	public static void main(String[] args) {
+		Random r=new Random();
+		r.setSeed(1);
+		for(int i=0;i<10;i++) {
+			System.out.println("value is "+r.nextInt());
+		}
+	}
+
+}
