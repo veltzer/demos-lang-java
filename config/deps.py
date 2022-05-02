c8aa446cd740c6f7c6680e7a2197e3b3d418d3cc
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-02 13:26:35 +0300

    better deps

--- config/deps.py
+++ config/deps.py
@@ -1,16 +1,19 @@
 packages=[
     # jdks and jres (for jmap, jhat, jconsole)
-    'openjdk-8-jre',
-    'openjdk-8-jdk',
-    'openjdk-8-doc',
-    #'openjdk-9-jre',
-    #'openjdk-9-jdk',
-    #'openjdk-9-doc',
-    # default jdk, jre
-    'default-jre',
-    'default-jre-headless',
-    'default-jdk',
-    'default-jdk-doc',
+    # 'openjdk-8-jre',
+    # 'openjdk-8-jdk',
+    # 'openjdk-8-doc',
+    # 'openjdk-9-jre',
+    # 'openjdk-9-jdk',
+    # 'openjdk-9-doc',
+    #  default jdk, jre
+    # 'openjdk-11-doc',
+    'openjdk-11-jre',
+    'openjdk-11-jdk',
+    # 'default-jre',
+    # 'default-jre-headless',
+    # 'default-jdk',
+    # 'default-jdk-doc',
     # libraries
     'libjna-java',
     # tools
