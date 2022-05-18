64ee0f49b7693ea9c8fb2292ccdc1cc8645dc6a7
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:56 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-java"
-project_long_description = "Demos for the java language"
-project_year_started = 2013
-project_description = """Demos for the java language"""
+description_short = "Demos for the java language"
