77472cb69f3a42146fc9db4449ff56b8872f4229
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-05 23:51:38 +0200

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -1,35 +1,23 @@
+java_version=22
+
 packages=[
     # jdks and jres (for jmap, jhat, jconsole)
-    # "openjdk-8-jre",
-    # "openjdk-8-jdk",
-    # "openjdk-8-doc",
-    # "openjdk-9-jre",
-    # "openjdk-9-jdk",
-    # "openjdk-9-doc",
-    #  default jdk, jre
-    # "openjdk-11-doc",
-    "openjdk-11-jre",
-    "openjdk-11-jdk",
-    # "default-jre",
-    # "default-jre-headless",
-    # "default-jdk",
-    # "default-jdk-doc",
-    # libraries
+    f"openjdk-{java_version}-jre-headless",
+    f"openjdk-{java_version}-jdk-headless",
+    # java libraries
     "libjna-java",
     # tools
-    # reason: for jvisualvm
     "visualvm",
     "ivy",
-    "ivy-doc",
     "ant",
-    "ant-doc",
     "ant-optional",
     "ant-contrib",
-    # maven stuff
     "maven",
-    # groovy stuff
     "groovy",
-    "groovy-doc",
+    "checkstyle",
+]
+
+packages_development_environments=[
     # development environments
     # "netbeans",
     # "eclipse-cdt",
@@ -41,11 +29,11 @@ packages=[
     # "eclipse-gef",
     # "eclipse-rcp",
     # "eclipse-xsd",
-    # java installers
-    # "oracle-java6-installer",
-    # "oracle-java7-installer",
-    # "oracle-java8-installer",
-    # checkstyle
-    "checkstyle",
+]
+
+package_docs=[
     "checkstyle-doc",
+    "groovy-doc",
+    "ant-doc",
+    "ivy-doc",
 ]
